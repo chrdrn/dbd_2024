a904255894f2a5edf1a7780a0c3f392cc3e26512
Christoph Adrian <christoph.adrian@fau.de>
2024-09-05 16:18:15 +0200

    Setup quarto (webpage) project

---
+++ quarto_extensions-installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install required Quarto extensions
+quarto add quarto-ext/fontawesome
+quarto add mcanouil/quarto-iconify
+quarto install extension schochastics/academicons
+quarto install extension shafayetShafee/bsicons
+quarto install extension jmbuhr/quarto-qrcode
+quarto install extension sellorm/quarto-social-embeds
+
+
